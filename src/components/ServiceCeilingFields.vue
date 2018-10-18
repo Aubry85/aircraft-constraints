ab7b8bb44cd718ab62b0834d8775b059f064a9dc
Aubry85 <44273213+Aubry85@users.noreply.github.com>
2018-10-18 14:31:26 -0400

    Update ServiceCeilingFields.vue
    Changed the denominator of the first term, since the formula was not correct. It needs to be sqrt(2/rho*W/S*sqrt(k/(3Cdmin))). You can verify if I wrote it properly.

--- src/components/ServiceCeilingFields.vue
+++ src/components/ServiceCeilingFields.vue
@@ -2,8 +2,8 @@
   <div>
     <fieldset>
       <legend>Service ceiling</legend>
-      <Equation data="$$\frac{W_{TO}}{S} = \frac{V_v}{\sqrt{\frac{2k}{3\rho C_{D_{min}}}
-        \cdot\frac{W_{TO}}{S}}} + 4\sqrt{\frac{k\cdot C_{D_{min}}}{3}}$$"/>
+      <Equation data="$$\frac{W_{TO}}{S} = \frac{V_v}{\sqrt{\frac{2}{\rho}
+        \cdot \sqrt{\frac{k}{3\cdot C_{D_{min}}}}\cdot \frac{W_{TO}}{S}}} + 4\sqrt{\frac{k\cdot C_{D_{min}}}{3}}$$"/>
       <label><Equation data="$\rho$" title="Density"/>
         <input type="range" min="0.0004" max=".003" step="0.0001"
           v-model.number="constraint.density"/>
